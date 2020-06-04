7bd731ada36267c7eadecb92511eedb65373e1ef
Zac Solomon <zac@zacsolo.com>
2020-06-04 19:28:04 -0400

    CRUD Routes and operations for Exercises

--- mern-exercise-tracker/backend/routes/exercises.js
+++ mern-exercise-tracker/backend/routes/exercises.js
@@ -23,7 +23,35 @@ router.route('/add').post((req, res) => {
   newExercise
     .save()
     .then(() => res.json('Exercise added!'))
-    .catch((err) => res.status(400).json('Error' + err));
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise Deleted'))
+    .catch((err) => res.status(400).json('Error:' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then((exercise) => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise
+        .save()
+        .then(() => res.json('Exercise Updated!'))
+        .catch((err) => res.status(400).json('Error: ' + err));
+    })
+    .catch((err) => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
