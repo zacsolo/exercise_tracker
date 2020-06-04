b7c441950b7a0fda43a16b9b14787a430f402f31
Zac Solomon <zac@zacsolo.com>
2020-06-04 15:38:16 -0400

    Create User and Exercise Routes

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -17,6 +17,13 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log('MongoDB database connection estalished successfully');
 });
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
